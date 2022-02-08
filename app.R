370d970768c10781b2154ced2a419b879d9b8c10
odmaclean <odm04w@gmail.com>
2022-02-07 20:43:54 -0500

    Fixed layout on at least my display. Added water, levain, etc. to table, and a section for levain flours.

--- app.R
+++ app.R
@@ -11,64 +11,147 @@ library(shiny)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
+  # tags$head(
+  #   tags$style(
+  #     HTML('
+  #       #uploadfile{height: 70px}
+  #       #rat{height: 60px}
+  #       #ratio{height: 60px}
+  #       #mytext{width: 50px}
+  #   ')
+  #   )
+  # ),
 
     # Application title
     titlePanel("Sourdough Calculator"),
 
     # Key parameters: flour weight and hydration percentage
-    numericInput(inputId = "total_flour",
-                 label = "Total flour weight (g)",
-                 value = 500),
-    numericInput(inputId = "hydration",
-                 label = "Hydration percentage",
-                 value = 75),
-    br(),
-    helpText("Set the percentages of flours 2 or 3 below",
-             "and the weights will calculate automatically,",
-             "with flour 1 as the remainder:"),
     fluidRow(
-      column(4,
-             h5("Flour:"),
-             h4("1 (e.g. white)"),
-             h4("2 (e.g. whole wheat)"),
-             h4("3 (e.g. rye)"),
-      ),
-      column(4,
-             h5("Percent:"),
-             textOutput("pct_flour_1", container = div),
-             numericInput(inputId = "pct_flour_2",
-                          label = NULL,
-                          value = 20),
-             numericInput(inputId = "pct_flour_3",
-                          label = NULL,
-                          value = 0),
-      ),
-      column(4,
-             h5("Weight (g):"),
-             textOutput("wt_flour_1", container = div),
-             textOutput("wt_flour_2", container = div),
-             textOutput("wt_flour_3", container = div),
-      )
+      column(4, numericInput(inputId = "total_flour",
+                 label = "Total flour weight (g)",
+                 value = 500)),
+      column(4, numericInput(inputId = "hydration",
+                 label = "Hydration percentage",
+                 value = 75)),
+      column(4, numericInput(inputId = "levain_hydro",
+                 label = "Levain hydration percentage",
+                 value = 100))
     ),
+    helpText("Percentages of flours 2 or 3 set percentage of flour 1."),
+    helpText("Note: levain percentage defined by total weight / flour weight"),
+    fluidRow(
+      column(4, h4(style="strong", "Ingredient")),
+      column(4, h4("Percent")),
+      column(4, h4("Weight (g)"))
+    ),
+    fluidRow(
+      column(4, div(style="height: 30px;", "Flour 1 (e.g. white)")),
+      column(4, textOutput("pct_flour_1")),
+      column(4, div(textOutput("wt_flour_1")))
+    ),
+    fluidRow(
+      column(4, div(style="margin-top: 9px;", "Flour 2 (e.g. whole wheat)")),
+      column(4, numericInput(inputId = "pct_flour_2",
+                             label = NULL,
+                             value = 20)),
+      column(4, div(style="margin-top: 9px;",textOutput("wt_flour_2")))
+    ),
+    fluidRow(
+      column(4, div(style="margin-top: 9px;", "Flour 3 (e.g. rye)")),
+      column(4, numericInput(inputId = "pct_flour_3",
+                             label = NULL,
+                             value = 5)),
+      column(4, div(style="margin-top: 9px;",textOutput("wt_flour_3")))
+    ),
+    fluidRow(
+      column(4, div(style="height: 30px;", "Water")),
+      column(4),
+      column(4, div(textOutput("wt_water")))
+    ),
+    fluidRow(
+      column(4, div(style="margin-top: 9px;", "Levain")),
+      column(4, numericInput(inputId = "pct_levain",
+                             label = NULL,
+                             value = 25, min = 0, max = 100)),
+      column(4, div(style="margin-top: 9px;",textOutput("wt_levain")))
+    ),
+    fluidRow(
+      column(4, div(style="margin-top: 9px;", "Salt")),
+      column(4, numericInput(inputId = "pct_salt",
+                             label = NULL,
+                             value = 2.0, step = 0.1)),
+      column(4, div(style="margin-top: 9px;",textOutput("wt_salt")))
+    ),
+    fluidRow(
+      column(4, div(style="margin-top: 9px;", "Yeast")),
+      column(4, numericInput(inputId = "pct_yeast",
+                             label = NULL,
+                             value = 0, step = 0.1, min = 0)),
+      column(4, div(style="margin-top: 9px;",textOutput("wt_yeast")))
+    ),
+    br(),
+    fluidRow(
+      column(4, div(style="height: 30px;", "Levain Flour 1")),
+      column(4, textOutput("pct_lf_1"))
+    ),
+    fluidRow(
+      column(4, div(style="margin-top: 9px;", "Levain Flour 2")),
+      column(4, numericInput(inputId = "pct_lf_2",
+                             label = NULL,
+                             value = 0))
+    ),
+    fluidRow(
+      column(4, div(style="margin-top: 9px;", "Levain Flour 3")),
+      column(4, numericInput(inputId = "pct_lf_3",
+                             label = NULL,
+                             value = 0))
+    )
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
   
-    pct_flour_1_int <- reactive({ 100 - input$pct_flour_2 - input$pct_flour_3 })
-    output$pct_flour_1 <- renderText({ pct_flour_1_int() })
-    output$wt_flour_1 <- renderText({ input$total_flour * pct_flour_1_int() / 100 })
-    output$wt_flour_2 <- renderText({ input$total_flour * input$pct_flour_2 / 100 })
-    output$wt_flour_3 <- renderText({ input$total_flour * input$pct_flour_3 / 100 })
+    pct_flour_calc <- reactive(
+      { 100 - input$pct_flour_2 - input$pct_flour_3 }
+      )
+    output$pct_flour_1 <- renderText({ pct_flour_calc() })
+    output$wt_flour_1 <- renderText({ 
+      round(input$total_flour * pct_flour_calc() / 100 - lf_1())
+      })
+    output$wt_flour_2 <- renderText({ 
+      round(input$total_flour * input$pct_flour_2 / 100 - lf_2()) })
+    output$wt_flour_3 <- renderText({ 
+      round(input$total_flour * input$pct_flour_3 / 100 - lf_3()) })
     
-    # output$distPlot <- renderPlot({
-    #     # generate bins based on input$bins from ui.R
-    #     x    <- faithful[, 2]
-    #     bins <- seq(min(x), max(x), length.out = input$bins + 1)
-    # 
-    #     # draw the histogram with the specified number of bins
-    #     hist(x, breaks = bins, col = 'darkgray', border = 'white')
-    # })
+    ## Levain math
+    # Levain weight and output
+    wt_levain_calc <- reactive(
+      { input$total_flour * input$pct_levain / 100 }
+    )
+    output$wt_levain <- renderText( { wt_levain_calc() } )
+    
+    # Levain flour and water weights
+    levain_flour_tot <- reactive(
+      { wt_levain_calc() / ( 1 + input$levain_hydro / 100 )}
+    )
+    levain_water <- reactive( { wt_levain_calc() - levain_flour_tot() } )
+    pct_lf_1_calc <- reactive({ 100 - input$pct_lf_2 - input$pct_lf_3 })
+    lf_1 <- reactive({ levain_flour_tot() * pct_lf_1_calc() / 100 })
+    lf_2 <- reactive({ levain_flour_tot() * input$pct_lf_2 / 100 })
+    lf_3 <- reactive({ levain_flour_tot() * input$pct_lf_3 / 100 })
+    output$pct_lf_1 <- renderText({ pct_lf_1_calc() })
+    
+    wt_water_calc <- reactive(
+      { round(input$total_flour * input$hydration / 100 - levain_water()) }
+    )
+    output$wt_water <- renderText({ wt_water_calc() })
+    
+    output$wt_salt <- renderText({ 
+      round(input$total_flour * input$pct_salt / 100, 1) })
+
+    output$wt_yeast <- renderText({ 
+      round(input$total_flour * input$pct_yeast / 100, 1) })
+    
 }
 
 # Run the application 
