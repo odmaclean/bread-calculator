54aa0e9e29c9484b925b8977caad8b81ebd19002
odmaclean <odm04w@gmail.com>
2022-02-09 16:42:14 -0500

    Fixed UI to work on mobile and desktop browsers. Added an output or two.

--- app.R
+++ app.R
@@ -1,161 +1,156 @@
 #
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
+# This is a Shiny web application to calculate ingredient weights in 
+# sourdough recipes.
+# 
+# Author: Oliver MacLean
+# Feb. 2022
+
+# Possible to-do list
+# - Add help tab
+# - Add save/load functionality: code in field?
+# - Add total weight tab, or even better way to update inputs
+# - Comment code
+# - Add extra ingredients
 
 library(shiny)
 
-# Define UI for application that draws a histogram
 ui <- fluidPage(
-  # tags$head(
-  #   tags$style(
-  #     HTML('
-  #       #uploadfile{height: 70px}
-  #       #rat{height: 60px}
-  #       #ratio{height: 60px}
-  #       #mytext{width: 50px}
-  #   ')
-  #   )
-  # ),
-
-    # Application title
-    titlePanel("Sourdough Calculator"),
-
-    # Key parameters: flour weight and hydration percentage
-    fluidRow(
-      column(3, numericInput(inputId = "total_flour",
-                 label = "Total flour weight (g)", width = '60%',
-                 value = 500)),
-      column(3, numericInput(inputId = "hydration",
-                 label = "Hydration percentage", width = '60%',
-                 value = 75)),
-      column(3, numericInput(inputId = "levain_hydro",
-                 label = "Levain hydration", width = '60%',
-                 value = 100))
-    ),
-    fluidRow(
-      column(6,
-           helpText("Percentages of flours 2 or 3 set percentage of flour 1."),
-           helpText("Note: levain percentage defined by total weight / flour weight")
-      )
-    ),
-    fluidRow(
-      column(3, h4(style="strong", "Ingredient")),
-      column(3, h4("Percent")),
-      column(3, h4("Weight (g)"))
-    ),
-    fluidRow(
-      column(3, div(style="height: 30px;", "Flour 1 (e.g. white)")),
-      column(3, textOutput("pct_flour_1")),
-      column(3, div(textOutput("wt_flour_1")))
-    ),
-    fluidRow(
-      column(3, div(style="margin-top: 9px;", "Flour 2 (e.g. whole wheat)")),
-      column(3, numericInput(inputId = "pct_flour_2",
-                             label = NULL, width = '60%',
-                             value = 20)),
-      column(3, div(style="margin-top: 9px;",textOutput("wt_flour_2")))
-    ),
-    fluidRow(
-      column(3, div(style="margin-top: 9px;", "Flour 3 (e.g. rye)")),
-      column(3, numericInput(inputId = "pct_flour_3",
-                             label = NULL, width = '60%',
-                             value = 5)),
-      column(3, div(style="margin-top: 9px;",textOutput("wt_flour_3")))
-    ),
-    fluidRow(
-      column(3, div(style="height: 30px;", "Water")),
-      column(3),
-      column(3, div(textOutput("wt_water")))
-    ),
-    fluidRow(
-      column(3, div(style="margin-top: 9px;", "Levain")),
-      column(3, numericInput(inputId = "pct_levain",
-                             label = NULL, width = '60%',
-                             value = 25, min = 0, max = 100)),
-      column(3, div(style="margin-top: 9px;",textOutput("wt_levain")))
-    ),
-    fluidRow(
-      column(3, div(style="margin-top: 9px;", "Salt")),
-      column(3, numericInput(inputId = "pct_salt",
-                             label = NULL, width = '60%',
-                             value = 2.0, step = 0.1)),
-      column(3, div(style="margin-top: 9px;",textOutput("wt_salt")))
-    ),
-    fluidRow(
-      column(3, div(style="margin-top: 9px;", "Yeast")),
-      column(3, numericInput(inputId = "pct_yeast",
-                             label = NULL, width = '60%',
-                             value = 0, step = 0.1, min = 0)),
-      column(3, div(style="margin-top: 9px;",textOutput("wt_yeast")))
-    ),
-    br(),
-    fluidRow(
-      column(3, div(style="height: 30px;", "Levain Flour 1")),
-      column(3, textOutput("pct_lf_1"))
-    ),
-    fluidRow(
-      column(3, div(style="margin-top: 9px;", "Levain Flour 2")),
-      column(3, numericInput(inputId = "pct_lf_2",
-                             label = NULL, width = '60%',
-                             value = 0))
-    ),
-    fluidRow(
-      column(3, div(style="margin-top: 9px;", "Levain Flour 3")),
-      column(3, numericInput(inputId = "pct_lf_3",
-                             label = NULL, width = '60%',
-                             value = 0))
+  tags$head(
+    tags$style(
+      HTML('
+        .shiny-text-output {
+          margin-left: 12px; margin-bottom: 6px;
+        }
+    ')
     )
+  ),
+  
+  # Application title
+  titlePanel("Sourdough Calculator"),
+  
+  # Key parameters: flour weight and hydration percentage
+  splitLayout(
+    numericInput(inputId = "total_flour",
+                 label = "Total flour (g)", width = '100%',
+                 value = 500),
+    numericInput(inputId = "hydration",
+                 label = "Hydration percent.", width = '100%',
+                 value = 75),
+  ),
+  tags$label(class="control-label;", "Total dough weight (g)"),
+  textOutput("wt_total"),
+  helpText("Percentages of flours 2 and 3 set percentage of flour 1."),
+  helpText("Note: levain percentage defined by total weight / flour weight"),
+  splitLayout(
+    h4("Percent"),
+    h4("Weight (g)")
+  ),
+  tags$label(class="control-label;", "Flour 1 (e.g. white)"),
+  splitLayout(
+    textOutput("pct_flour_1"),
+    textOutput("wt_flour_1")
+  ),
+  splitLayout(
+    numericInput(inputId = "pct_flour_2",
+                 label = "Flour 2 (e.g. whole)", 
+                 width = '100%', value = 20),
+    div(style="margin-top: 30px;",textOutput("wt_flour_2"))
+  ),
+  splitLayout(
+    numericInput(inputId = "pct_flour_3",
+                 label = "Flour 3 (e.g rye)", width = '100%',
+                 value = 5),
+    div(style="margin-top: 30px;",textOutput("wt_flour_3"))
+  ),
+  tags$label(class="control-label;", "Water"),
+  splitLayout(
+    div(textOutput("pct_water")),
+    div(textOutput("wt_water"))
+  ),
+  splitLayout(
+    numericInput(inputId = "pct_levain",
+                 label = "Levain", width = '100%',
+                 value = 25, min = 0, max = 100),
+    div(style="margin-top: 30px;",textOutput("wt_levain"))
+  ),
+  splitLayout(
+    numericInput(inputId = "pct_salt",
+                 label = "Salt", width = '100%',
+                 value = 2.0, step = 0.1),
+    div(style="margin-top: 30px;",textOutput("wt_salt"))
+  ),
+  splitLayout(
+    numericInput(inputId = "pct_yeast",
+                 label = "Yeast", width = '100%',
+                 value = 0, step = 0.1, min = 0),
+    div(style="margin-top: 34px;",textOutput("wt_yeast"))
+  ),
+  br(),
+  h4("Levain Parameters:"),
+  splitLayout(
+    numericInput(inputId = "levain_hydro",
+                 label = "Levain hydration", width = '100%',
+                 value = 100),
+    div(tags$label(class="control-label;", "Levain Flour 1"),
+        div(style="margin-top: 6px;", textOutput("pct_lf_1")))
+  ),
+  splitLayout(
+    numericInput(inputId = "pct_lf_2",
+                 label = "Levain Flour 2", width = '100%',
+                 value = 0),
+    numericInput(inputId = "pct_lf_3",
+                 label = "Levain Flour 3", width = '100%',
+                 value = 0)
+  )
 )
 
-# Define server logic required to draw a histogram
 server <- function(input, output) {
   
-    pct_flour_calc <- reactive(
-      { 100 - input$pct_flour_2 - input$pct_flour_3 }
-      )
-    output$pct_flour_1 <- renderText({ pct_flour_calc() })
-    output$wt_flour_1 <- renderText({ 
-      round(input$total_flour * pct_flour_calc() / 100 - lf_1())
-      })
-    output$wt_flour_2 <- renderText({ 
-      round(input$total_flour * input$pct_flour_2 / 100 - lf_2()) })
-    output$wt_flour_3 <- renderText({ 
-      round(input$total_flour * input$pct_flour_3 / 100 - lf_3()) })
-    
-    ## Levain math
-    # Levain weight and output
-    wt_levain_calc <- reactive(
-      { input$total_flour * input$pct_levain / 100 }
-    )
-    output$wt_levain <- renderText( { wt_levain_calc() } )
-    
-    # Levain flour and water weights
-    levain_flour_tot <- reactive(
-      { wt_levain_calc() / ( 1 + input$levain_hydro / 100 )}
-    )
-    levain_water <- reactive( { wt_levain_calc() - levain_flour_tot() } )
-    pct_lf_1_calc <- reactive({ 100 - input$pct_lf_2 - input$pct_lf_3 })
-    lf_1 <- reactive({ levain_flour_tot() * pct_lf_1_calc() / 100 })
-    lf_2 <- reactive({ levain_flour_tot() * input$pct_lf_2 / 100 })
-    lf_3 <- reactive({ levain_flour_tot() * input$pct_lf_3 / 100 })
-    output$pct_lf_1 <- renderText({ pct_lf_1_calc() })
-    
-    wt_water_calc <- reactive(
-      { round(input$total_flour * input$hydration / 100 - levain_water()) }
-    )
-    output$wt_water <- renderText({ wt_water_calc() })
-    
-    output$wt_salt <- renderText({ 
-      round(input$total_flour * input$pct_salt / 100, 1) })
-
-    output$wt_yeast <- renderText({ 
-      round(input$total_flour * input$pct_yeast / 100, 1) })
-    
+  pct_flour_calc <- reactive(
+    { 100 - input$pct_flour_2 - input$pct_flour_3 }
+  )
+  output$pct_flour_1 <- renderText({ pct_flour_calc() })
+  output$wt_flour_1 <- renderText({ 
+    round(input$total_flour * pct_flour_calc() / 100 - lf_1())
+  })
+  output$wt_flour_2 <- renderText({ 
+    round(input$total_flour * input$pct_flour_2 / 100 - lf_2()) })
+  output$wt_flour_3 <- renderText({ 
+    round(input$total_flour * input$pct_flour_3 / 100 - lf_3()) })
+  
+  ## Levain math
+  # Levain weight and output
+  wt_levain_calc <- reactive(
+    { input$total_flour * input$pct_levain / 100 }
+  )
+  output$wt_levain <- renderText( { wt_levain_calc() } )
+  
+  # Levain flour and water weights
+  levain_flour_tot <- reactive(
+    { wt_levain_calc() / ( 1 + input$levain_hydro / 100 )}
+  )
+  levain_water <- reactive( { wt_levain_calc() - levain_flour_tot() } )
+  pct_lf_1_calc <- reactive({ 100 - input$pct_lf_2 - input$pct_lf_3 })
+  lf_1 <- reactive({ levain_flour_tot() * pct_lf_1_calc() / 100 })
+  lf_2 <- reactive({ levain_flour_tot() * input$pct_lf_2 / 100 })
+  lf_3 <- reactive({ levain_flour_tot() * input$pct_lf_3 / 100 })
+  output$pct_lf_1 <- renderText({ pct_lf_1_calc() })
+  
+  wt_water_calc <- reactive(
+    { round(input$total_flour * input$hydration / 100 - levain_water()) }
+  )
+  output$wt_water <- renderText({ wt_water_calc() })
+  output$pct_water <- renderText({ input$hydration })
+  
+  output$wt_salt <- renderText({ 
+    round(input$total_flour * input$pct_salt / 100, 1) })
+  
+  output$wt_yeast <- renderText({ 
+    round(input$total_flour * input$pct_yeast / 100, 1) })
+  
+  output$wt_total <- renderText({ 
+    round(input$total_flour * (100 + input$hydration + input $pct_salt + 
+                                 input$pct_yeast) / 100, 1) })
 }
 
 # Run the application 
